cdbe660d6ec70df4d2aaaabe83bbfa11bb503093
Arijan Demaj <arijandemaj01@gmail.com>
2025-07-08 19:09:58 +0200

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 
+import { Analytics } from '@vercel/analytics/react';
+
 function App() {
   return (
     <>
@@ -18,6 +20,7 @@ function App() {
         </Routes>
       </main>
       <Footer /> 
+      <Analytics/>
     </>
   );
 }

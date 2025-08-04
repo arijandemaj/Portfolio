50c6e6cfcf0af828a1dc7c2ea12db5ed1f24f5f4
Arijan Demaj <arijandemaj01@gmail.com>
2025-08-04 18:52:05 +0200

    Add Loading Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,44 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useSearchParams } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import TerminalLoadingScreen from './components/TerminalLoadingScreen';
 
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 
 import { Analytics } from '@vercel/analytics/react';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </main>
-      <Footer /> 
-      <Analytics/>
-    </>
-  );
+
+  const [isLoading, setIsLoading] = useState(true)
+
+  if(isLoading){
+    return (
+      <>
+        <TerminalLoadingScreen onAnimationFinish={(done) => setIsLoading(!done) } />
+        <Analytics/>
+      </>
+    );
+  }
+  else{
+   return (
+      <>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </main>
+        <Footer /> 
+        <Analytics/>
+      </>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/TerminalLoadingScreen.jsx
@@ -0,0 +1,108 @@
+import { useEffect, useRef, useState } from "react";
+import linuxWindowIcons from '/icons/linux-mint-icons.png'
+
+
+function TerminalLoadingScreen({onAnimationFinish}){
+
+    const machineName = "arijan@computer"
+    const [cdCommad, setCdCommand] = useState('')
+    const cdCommandIntervalRef = useRef(null);
+    const cdCommandIndexRef = useRef(0)
+    
+    const openVSCodeCommandContainerRef = useRef(null);
+    const vsCodeCommandIntervalRef = useRef(null);
+    const [openVSCodeCommand, setOpenVSCodeCommand] = useState('')
+    const openVSCodeCommandIndexRef = useRef(0);
+    const commandCursorRef = useRef(null);
+
+    
+
+    useEffect(() => {
+
+        const cdCommandText = "cd portfolio";
+        const openVSCodeCommandText = "code ."
+
+        const cdCommandFunction = () => {
+                
+            cdCommandIntervalRef.current = setInterval(() => {
+            
+                if(cdCommandIndexRef.current < cdCommandText.length)
+                {
+                    const nextChar = cdCommandText[cdCommandIndexRef.current];
+                    setCdCommand((prev) => prev + nextChar)
+                    cdCommandIndexRef.current += 1;
+                }
+                else
+                {
+                    clearInterval(cdCommandIntervalRef.current)
+                    openVSCodeCommandContainerRef.current.style.visibility = "visible"
+                    commandCursorRef.current.style.visibility = "hidden"
+                    setTimeout(() => {
+                        openVSCodeCommandFunction();
+                    }, 200);
+                }
+
+            }, 100);
+        }
+        
+        const openVSCodeCommandFunction = () => {
+                
+            vsCodeCommandIntervalRef.current = setInterval(() => {
+            
+                if(openVSCodeCommandIndexRef.current < openVSCodeCommandText.length)
+                {
+                    const nextChar = openVSCodeCommandText[openVSCodeCommandIndexRef.current];
+                    setOpenVSCodeCommand((prev) => prev + nextChar)
+                    openVSCodeCommandIndexRef.current += 1;
+                }
+                else
+                {
+                    clearInterval(vsCodeCommandIntervalRef.current)
+                    setTimeout(() => {
+                        onAnimationFinish(true)
+                    }, 1000)
+                    
+                }
+
+            }, 100);
+        }
+
+
+        cdCommandFunction();
+
+        return () => {
+            
+            clearInterval(cdCommandIntervalRef.current);
+            clearInterval(vsCodeCommandIntervalRef.current)
+          
+        }
+
+    }, [])
+
+    return (
+        <div id="terminal-container">
+            <div id="terminal-window">
+                <div id="terminal-header">
+                    <p id="terminal-title">{machineName} ~</p>
+                    <img src={linuxWindowIcons} width="80" alt="linux-mint-icons" id="linux-mint-icons"/>
+                </div>
+                <div id="terminal-body">
+                    <div className='terminal-commands'>
+                        <p className="terminal-command-title">{machineName}</p>
+                        <p className='terminal-pre-command'>~</p>
+                        <p className='typed-command'>$ {cdCommad}</p>
+                        <span ref={commandCursorRef} className="cursor">|</span>
+                    </div>
+                    <div className='terminal-commands' ref={openVSCodeCommandContainerRef} style={{visibility: 'hidden'}}>
+                        <p className="terminal-command-title">{machineName}</p>
+                        <p className='terminal-pre-command'>~/portfolio</p>
+                        <p className='typed-command'>$ {openVSCodeCommand}</p>
+                        <span className="cursor">|</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TerminalLoadingScreen;
